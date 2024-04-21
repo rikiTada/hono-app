ae3b7d2a5c9e6dc84393c699daeaf8efe5676833
rikiTada <riki.tada777@gmail.com>
2024-04-21 21:55:52 +0900

    CRUD作成

---
+++ src/api/users.ts
@@ -0,0 +1,22 @@
+export const users = [
+  {
+    id: '1',
+    name: 'john',
+    content: '',
+  },
+  {
+    id: '2',
+    name: 'taro',
+    content: '',
+  },
+  {
+    id: '3',
+    name: 'tanaka',
+    content: '',
+  },
+  {
+    id: '4',
+    name: 'ono',
+    content: '',
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,69 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { html } from './html';
+import { users } from './api/users';
+import { prettyJSON } from 'hono/pretty-json';
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono();
+
+app.use('*', prettyJSON());
+
+app.get('/', async (c) => {
+  return c.html(html);
+});
+
+app.get('/api/users', async (c) => {
+  return c.json({ users });
+});
+
+app.get('/api/users/:id', async (c) => {
+  const id = c.req.param('id');
+  const user = users.find((u) => u.id === id);
+
+  if (user) {
+    return c.json({ user });
+  } else {
+    return c.json({ error: 'user not found.' }, 404);
+  }
+});
+
+app.post('/api/users', async (c) => {
+  const { name, content } = await c.req.json<{ name: string; content: string }>();
+
+  const newUser = {
+    id: String(users.length + 1),
+    name,
+    content,
+  };
+
+  users.push(newUser);
+  console.log(users);
+  return c.json({ user: newUser }, 201);
+});
+
+app.put('/api/users/:id', async (c) => {
+  const id = c.req.param('id');
+  const index = users.findIndex((u) => u.id === id);
+
+  if (index === -1) {
+    return c.json({ error: 'user not found.' }, 404);
+  }
+
+  const { name, content } = await c.req.json();
+  users[index] = { ...users[index], name, content };
+
+  return c.json(users[index]);
+});
+
+app.delete('/api/users/:id', async (c) => {
+  const id = c.req.param('id');
+  const index = users.findIndex((u) => u.id === id);
+
+  if (index === -1) {
+    return c.json({ error: 'user not found.' }, 404);
+  }
+
+  users.splice(index, 1);
+  return c.json(users);
+});
+
+export default app;
