ec4890080d4e0a7f6fa51902886e52f50e2be86d
rikiTada <riki.tada777@gmail.com>
2024-04-22 00:20:50 +0900

    authへログイン追加

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono';
-import { html } from '../html';
+import { basicAuth } from 'hono/basic-auth';
+
 const app = new Hono();
 
-app.get('/page', async (c) => {
-  return c.html(html);
+// `http://127.0.0.1:8787/auth/*`にログインを求める場合
+app.use(
+  '/*',
+  basicAuth({
+    username: 'admin',
+    password: 'admin',
+  })
+);
+
+app.get('/', async (c) => {
+  return c.html(`<h1>Hello World</h1>`);
 });
 
 export default app;
